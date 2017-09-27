6c34f844d1a986b09804622bb792858880891818
podcastm1 <podcastm1@gmail.com>
2017-09-27 22:58:15 +0900

    add module list

--- TIC/TIC.ino
+++ TIC/TIC.ino
@@ -3,12 +3,10 @@
 SoftwareSerial bleCon(2,3); // Rx, Tx
 
 void setup() {
-  // put your setup code here, to run once:
   bleCon.begin(9600);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
